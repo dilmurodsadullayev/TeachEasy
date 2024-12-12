7e8b849b91d8d9d27c5ac1fd93862f9c6a4d8315
Dilmurod <142794823+d1ilmurod@users.noreply.github.com>
2024-12-12 19:50:58 +0500

    change packeg

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os,dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import dj_database_url
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
